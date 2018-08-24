a0c8046531505a556089815458ef56c93bf9587e
caozhijie <dearzhijie@gmail.com>
2018-08-24 16:39:57 +0800

    first add

--- main.cpp
+++ main.cpp
@@ -10,24 +10,52 @@ int main(int argc, char** argv )
 {
     String image_path="/home/czj/Project/crowd_counting_cpp/test_images/frame_01120.jpg";
     String model_path="/home/czj/Project/crowd_counting_cpp/hongqiao_dt5_lr1e5_bs1_ep50.pb";
-    Mat img=imread(image_path,0);
+    String video_path="/home/czj/Videos/videos4/stream0.mp4";
+    CrowdCounter crowdCounter(model_path);
 
-
-    Mat heatmap;
     double num=0;
     int kernel_size=11;
     int sigma=4;
     int enlarge_rate=2;
 
+      ///用图片测试
+//    Mat img=imread(image_path,0);
+//    Mat heatmap;
 
-    CrowdCounter crowdCounter(model_path);
-    crowdCounter.process(img,heatmap,num,kernel_size,sigma,enlarge_rate);
+//    crowdCounter.process(img,heatmap,num,kernel_size,sigma,enlarge_rate);
+//    imshow("heatmap",heatmap);
+//    cout<<"num people:"<<num<<endl;
+//    waitKey(0);
 
-    //显示heatmap
-    imshow("heatmap",heatmap);
-    cout<<"num of people:"<<num<<endl;
+      ///用视频测试
+    VideoCapture cap(video_path);
+    if(!cap.isOpened())
+    {
+        cout<<"Can't open the video"<<endl;
+        return -1;
+    }
+    for(;;)
+    {
+        Mat img;
+        cap>>img;
+        Mat heatmap;
+        Mat gray;
+        cvtColor(img,gray,CV_BGR2GRAY);
+        crowdCounter.process(gray,heatmap,num,kernel_size,sigma,enlarge_rate);
 
-    waitKey(0);
+        //显示heatmap
+        imshow("heatmap",heatmap);
+        cout<<"num of people:"<<num<<endl;
+
+        //在图片上显示文字
+        resize(img,img,Size(1280,720));
+        putText(img,format("num peole:%d",int(num)),Point(50,80),CV_FONT_NORMAL,3,Scalar(0,0,255),2,8);
+        imshow("1",img);
+        int k=waitKey(1);
+        if(k==27)
+            break;
+    }
+
     return 0;
 }
 
